636b0ab96866af216e88a76fdafe380108398e45
dominikpin <dp4550@student.uni-lj.si>
2023-12-07 13:17:51 +0100

    day 4 complited

---
+++ day04/Problem_1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "md5.h"
+
+int main() {
+    std::fstream input;
+	input.open("day04\\input.txt");
+    std::string text;
+    getline(input, text);
+    int i = 1;
+    while (true) {
+        std::string newString = text + std::to_string(i);
+        if (GetMD5String(newString).substr(0, 5) == "00000") {
+            std::cout << i << std::endl;
+            break;
+        }
+        i++;
+    }
+    input.close(); 
+}
